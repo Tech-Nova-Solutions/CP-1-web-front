dc585df7352764e4c16750834abc3072e65e34bc
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-03 16:44:43 -0300

    Criando a estrutura do projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from './components/Nav'
+import {Outlet} from 'react-router-dom'
+import Footer from './components/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App

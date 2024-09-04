980f46b3ae6b5231e41fe6a88410ab0b2d1a38dd
Gustavo Viega Martins Lopes <gvmlopes@gmail.com>
2024-09-04 15:44:11 -0300

    Criando a pagina Produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -5,10 +5,60 @@ const Produtos = () => {
 
     <>
 
-    <div>
-      <h1>Produtos</h1>
-    </div>
+    <div className='container_produtos'>
+      <div>
+        <div className='produtos'>
+          <img className='bola1' src='/src/assets/Bola.jpg' alt='Produto 1' />
+          <div className='produto1'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
 
+        <div className='produtos'>
+          <img className='bola2' src='/src/assets/Bola2.jpg' alt='Produto 1' />
+          <div className='produto2'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
+
+        <div className='produtos'>
+          <img className='bola3' src='/src/assets/Bola3.jpg' alt='Produto 3' />
+          <div className='produto3'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
+      </div>
+      
+
+      <div className='produtos'>
+          <img className='bicicleta3' src='/src/assets/Bicicleta1.jpg' alt='Produto 4' />
+          <div className='produto4'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
+
+        <div className='produtos'>
+          <img className='bicicleta3' src='/src/assets/Bicicleta2.jpg' alt='Produto 5' />
+          <div className='produto5'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
+
+        <div className='produtos'>
+          <img className='bicicleta3' src='/src/assets/Bicicleta3.jpg' alt='Produto 6' />
+          <div className='produto6'>
+            <h3>Nome do produto</h3>
+            <p>Preco do produto</p>
+          </div>
+        </div>
+
+
+    </div>
 
 
     </>

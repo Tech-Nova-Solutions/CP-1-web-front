32c007b6a00a57edb17186f25b38e115986947ab
Felipe <fm043445@gmail.com>
2024-09-05 13:02:49 -0300

    Estilizando o Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FooterStyle } from '../css/FooterStyle'; // Importando o FooterStyle
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <FooterStyle>
       <div className="footer-section">
-        <h2 className="footer-title">Nome da Loja</h2>
-      </div>
-      
-      <div className="footer-section">
-        <Link to="/sobre" className="footer-link">Sobre Nós</Link>
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="footer-link">Instagram</a>
-        <a href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer" className="footer-link">WhatsApp</a>
-      </div>
-      
-      <div className="footer-section">
-        <h2 className="footer-title">Compras</h2>
-        <a href="#" className="footer-link">Termos de Compra</a>
-        <a href="#" className="footer-link">Garantia</a>
-        <a href="#" className="footer-link">Recomendações</a>
-      </div>
-      
-      <div className="footer-section">
-        <h2 className="footer-title">Fale Conosco</h2>
-        <Link to="/contato" className="footer-link">Contato</Link>
-        <a className="footer-link">atendimento@nomedaloja.com</a>
-        <a className="footer-link">(11)1234-5678</a>
+        <div>
+          <h2 className="footer-title">Nome da Loja</h2>
+          <Link to="/sobre" className="footer-link">Sobre Nós</Link>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="footer-link">Instagram</a>
+          <a href="https://www.whatsapp.com/" target="_blank" rel="noopener noreferrer" className="footer-link">WhatsApp</a>
+        </div>
+        
+        <div>
+          <h2 className="footer-title">Compras</h2>
+          <a href="#" className="footer-link">Termos de Compra</a>
+          <a href="#" className="footer-link">Garantia</a>
+          <a href="#" className="footer-link">Recomendações</a>
+        </div>
+        
+        <div>
+          <h2 className="footer-title">Fale Conosco</h2>
+          <Link to="/contato" className="footer-link">Contato</Link>
+          <a href="mailto:atendimento@nomedaloja.com" className="footer-link">atendimento@nomedaloja.com</a>
+          <a href="tel:+551112345678" className="footer-link">(11)1234-5678</a>
+        </div>
       </div>
 
       <div className="footer-copyright">
         <h5>@2024 - Todos os direitos reservados.</h5>
       </div>
-    </footer>
+    </FooterStyle>
   );
 };
 

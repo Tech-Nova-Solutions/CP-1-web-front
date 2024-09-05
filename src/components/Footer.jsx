5632d5a2d7255f4530b723d78ed03422e7344b72
Felipe <fm043445@gmail.com>
2024-09-05 12:13:53 -0300

    Criando o footer da pagina

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <>
-        <h5>@2024 - Todos os direitos reservados.</h5>
+      <footer>
+        <div>
+          <div>
+            <h2>Nome da Loja</h2>
+          </div>
+          <div>
+            <Link to='/sobre'>Sobre Nós</Link>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer">WhatsApp</a>
+          </div>
+          <div>
+            <h3>Compras</h3>
+          </div>
+          <div>
+            <a href="/termos-de-compra">Termos de Compra</a>
+            <a href="/garantia">Garantia</a>
+            <a href="/recomendacoes">Recomendações</a>
+          </div>
+          <div>
+            <h3>Fale Conosco</h3>
+          </div>
+          <div>
+            <Link to='/contato'>Contato</Link>
+            <a href="mailto:atendimento@nomedaloja.com">atendimento@nomedaloja.com</a>
+            <a href="tel:+551112345678">(11)1234-5678</a>
+          </div>
+        </div>
+        <h5>&copy; 2024 - Todos os direitos reservados.</h5>
+      </footer>
     </>
   )
 }
